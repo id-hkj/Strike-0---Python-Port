c2ca4f01b2411a5de30988aef3b28f46363dce89
id-hkj <83733473+id-hkj@users.noreply.github.com>
2021-05-16 23:12:10 +0100

    BIG UPDATE
    This now has the main character, Jumping Code, and is Object Oriented. I will be adding/improving the boundaries code, and will In a few weeks add a title screen. The Object Oriented approach will make things easier. Time to compile the .exe file to put in this folder!

--- Strike 0/Strike 0.py
+++ Strike 0/Strike 0.py
@@ -1,74 +1,122 @@
 # ©id-hkj 2021
 
 import pygame
+import os
 pygame.init()
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-pygame.display.set_icon(pygame.image.load(r"C:\Program Files (x86)\Strike 0\Sprites\favicon.png"))
+pygame.display.set_icon(pygame.image.load(os.path.join('Sprites', 'favicon.png')))
 win = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("Strike 0")
 
 #VARIABLES
-ScreenX, ScreenY = win.get_size()
+SCREENX, SCREENY = win.get_size()
+BLOCK_SET_1_WIDTH, BLOCK_SET_1_HEIGHT = int(2.664*SCREENX), int(SCREENY * 0.648)
+
+Backdrop = 1
+Unlocked = False
 scrollX = 0
 scrollY = 0
-Block_A_Width = 200
-Block_A_Height = 100
 Scroll_Speed = 10
 Shooting = False
 run = True
 
-print(ScreenX)
-print(ScreenY)
+print(SCREENX)
+print(SCREENY)
+
 
 #IMAGE LOADS/ANIMATION VARS
-bg_UNSCALED = pygame.image.load(r"C:\Program Files (x86)\Strike 0\Sprites\Bg.png").convert ()
-#Shoot_Animation_Left_UNSCALED = [pygame.image.load(r"C:\Program Files (x86)\Strike 0\Sprites\Hero\ReadyPos_Left.png"), pygame.image.load(r"C:\Program Files\Strike 0\Sprites\Hero\ShootingLift1_Left.png"), pygame.image.load(r"C:\Program Files\Strike 0\Sprites\Hero\ShootingLift2_Left.png"), pygame.image.load(r"C:\Program Files\Strike 0\Sprites\Hero\Lifted_Left.png")]
-#Shoot_Animation_Right_UNSCALED = [pygame.image.load(r"C:\Program Files (x86)\Strike 0\Sprites\Hero\ReadyPos_Right.png"), pygame.image.load(r"C:\Program Files\Strike 0\Sprites\Hero\ShootingLift1_Right.png"), pygame.image.load(r"C:\Program Files\Strike 0\Sprites\Hero\ShootingLift2_Right.png"), pygame.image.load(r"C:\Program Files\Strike 0\Sprites\Hero\Lifted_Right.png")]
-#Walking_Left_UNSCALED = pygame.image.load(r"C:\Program Files (x86)\Strike 0\Sprites\Hero\ReadyPos_Left.png")
-#Walking_Right_UNSCALED = pygame.image.load(r"C:\Program Files (x86)\Strike 0\Sprites\Hero\ReadyPos_Right.png")
-Blocks_Set_1_UNSCALED = pygame.image.load(r"C:\Program Files (x86)\Strike 0\Sprites\Blocks\Map_NoScrollY.png").convert_alpha()
+bg_Gameplay_UNSCALED = pygame.image.load(os.path.join('Sprites', 'Bg.png')).convert()
+Hero_Left_UNSCALED = [pygame.image.load(os.path.join('Sprites', 'Hero', 'ReadyPos_Left.png')).convert_alpha(), pygame.image.load(os.path.join('Sprites', 'Hero', 'ShootingLift1_Left.png')).convert_alpha, pygame.image.load(os.path.join('Sprites', 'Hero', 'ShootingLift2_Left.png')).convert_alpha, pygame.image.load(os.path.join('Sprites', 'Hero', 'Lifted_Left.png')).convert_alpha()]
+Hero_Right_UNSCALED = [pygame.image.load(os.path.join('Sprites', 'Hero', 'ReadyPos_Right.png')).convert_alpha(), pygame.image.load(os.path.join('Sprites', 'Hero', 'ShootingLift1_Right.png')).convert_alpha(), pygame.image.load(os.path.join('Sprites', 'Hero', 'ShootingLift2_Right.png')).convert_alpha(), pygame.image.load(os.path.join('Sprites', 'Hero', 'Lifted_Right.png')).convert_alpha()]
+Blocks_Set_1_UNSCALED = pygame.image.load(os.path.join('Sprites', 'Blocks', 'Map_NoScrollY.png')).convert_alpha()
 
 #IMAGE SCALINGS
-Blocks_Set_1 = pygame.transform.scale(Blocks_Set_1_UNSCALED, (int(2.664*ScreenX), int(ScreenY * 0.648)))
+Blocks_Set_1 = pygame.transform.scale(Blocks_Set_1_UNSCALED, (BLOCK_SET_1_WIDTH, BLOCK_SET_1_HEIGHT))
+bg_Gameplay = pygame.transform.scale(bg_Gameplay_UNSCALED, (SCREENX, SCREENY))
 
-bg = pygame.transform.scale(bg_UNSCALED, (ScreenX, ScreenY))
 
-Left = False
-Right = True
-ShootCount = 0
 clock = pygame.time.Clock()
 
-print(int(ScreenY * 0.5556))
+#OBJECTS
+class Player(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.Right = True
+        self.ShootCount = 0
+        self.Shooting = False
+        self.UpCount = 100
+    def Drawing(self, win):
+        self.win = win
+        #Blocks
+        self.win.blit(Blocks_Set_1, (scrollX - int(BLOCK_SET_1_WIDTH * 0.137), scrollY + int(SCREENY * 0.361)))
+        #Hero
+        if (Hero.Right == True):
+            self.win.blit(Hero_Right_UNSCALED[0], (Hero.x, Hero.y))
+        if (Hero.Right == False):
+            self.win.blit(Hero_Left_UNSCALED[0], (Hero.x, Hero.y))
+
+#SPRITES
+Hero = Player(int(SCREENX*0.28038), 0)
 
 #FUNCTIONS
-def Drawing():
-    #Blocks
-    win.blit(bg, (0, 0))
-    win.blit(Blocks_Set_1, (scrollX - 700, scrollY + int(ScreenY * 0.361)))
-    #win.blit(Blocks_Set_1, (scrollX - 700, scrollY + 390))
-    pygame.display.update()
+def Gameplay():
+    #Time to make a bunch of GLOBAL variables!
+    #HOORAY!
+
+    global Hero
+    global scrollX
+    global scrollY
+
+    #Ok, It wasn't that many.
+
+    #BOUNDARIES
+    if Hero.y <= 200 and Hero.UpCount == 100:
+        Hero.y += 10
+
+    #KEY PRESSED EVENTS
+    if KEYS[pygame.K_LEFT]:
+        scrollX += Scroll_Speed
+        Hero.Right = False
+    if KEYS[pygame.K_RIGHT]:
+        scrollX -= Scroll_Speed
+        Hero.Right = True
+    if KEYS[pygame.K_UP] and Hero.UpCount == 100:
+        Hero.UpCount = 25
     
+    if Hero.UpCount <= 25 and Hero.UpCount > -25:
+        if Hero.UpCount <= 0:
+            neg = -1
+        else:
+            neg = 1
+        Hero.y -= (10 * neg)
+        Hero.UpCount -= 1
+    else:
+        Hero.UpCount = 100
+    
+    if KEYS[pygame.K_SPACE] and Hero.Shooting == False:
+        Hero.Shooting = True
+
+    win.blit(bg_Gameplay, (0, 0))
+    Hero.Drawing(win)
+    pygame.display.update()
 
 #MAIN GAME LOOP
 while run:
     clock.tick(27)
+    KEYS = pygame.key.get_pressed()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    
-    #KEY PRESSED EVENTS
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_ESCAPE]:
+    if KEYS[pygame.K_ESCAPE]:
         run = False
-    if keys[pygame.K_LEFT]:
-        scrollX += Scroll_Speed
-    if keys[pygame.K_RIGHT]:
-        scrollX -= Scroll_Speed
-    if keys[pygame.K_UP]:
-        scrollY -= Scroll_Speed
-    if keys[pygame.K_SPACE]:
-        Shooting = True
-    Drawing()    
+
+    if Backdrop == 1:
+        Gameplay()
+    else:
+        print('Error 002: BadValue[Scene_NumberState-Invalid]')
+
 pygame.quit()
